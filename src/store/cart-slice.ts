cec34b73691e90da03104690186c95a2b4d22d88
nirgluzman <nir.gluzman@gmail.com>
2023-12-04 07:43:48 +0100

    implement 'removeFromCart' for removing items from the cart

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -21,12 +21,12 @@ export const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
 	reducers: {
-		// adding items to the cart.
+		// adding items to the shopping cart.
 		addToCart(
 			state,
 			action: PayloadAction<{id: string; title: string; price: number}> // PayloadAction has the type defintion of type of data we expect to be attached to this action.
 		) {
-			// checking if a similar item is already in the shopping cart.
+			// checking if a similar item is already in the cart.
 			// findIndex() method returns the index of the first element in an array that satisfies the provided testing function.
 			// if no elements satisfy the testing function, -1 is returned.
 			const itemIndex = state.items.findIndex((item) => item.id === action.payload.id);
@@ -39,6 +39,24 @@ export const cartSlice = createSlice({
 				state.items.push({...action.payload, quantity: 1});
 			}
 		},
-		removeFromCart() {},
+
+		// removing items from the shopping cart.
+		removeFromCart(
+			state,
+			action: PayloadAction<string> // for removing item we just need the 'id' (which is of type 'string').
+		) {
+			// find the item by index.
+			// findIndex() method returns the index of the first element in an array that satisfies the provided testing function.
+			// if no elements satisfy the testing function, -1 is returned.
+			const itemIndex = state.items.findIndex((item) => item.id === action.payload);
+
+			if (state.items[itemIndex].quantity === 1) {
+				// if item's quantity=1, then we need to remove the item from cart.
+				state.items.splice(itemIndex, 1);
+			} else {
+				// if item's quantity>1, then we need to reduce the quantity by 1.
+				state.items[itemIndex].quantity--;
+			}
+		},
 	},
 });
